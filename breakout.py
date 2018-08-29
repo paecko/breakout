fcfa3765100c6abae3df8bc23931e830a838c798
sunnep <dijkstrapapapa@gmail.com>
2018-08-29 19:29:50 -0400

    change_level uses only eval() for level changes. added comments and formatting

--- breakout.py
+++ breakout.py
@@ -8,25 +8,30 @@ from pygame.sprite import Group
 from play_button import Button
 from scoreboard import Scoreboard
 
+
 def run_game():
     pygame.init()
-
+    # get settings and set up screen
     gs = Settings()
     screen = pygame.display.set_mode((gs.screen_width,gs.screen_height))
-    screen_rect = screen.get_rect()
-    play_button = Button(screen,"Play")
-    scoreboard = Scoreboard(screen,gs)
-    paddle = Paddle(screen,gs)
-    ball = Ball(screen,gs,paddle)
+    screen_rect = screen.get_rect()  # get dimensions of screen
+    # following initializes Play Button, score label, paddle, ball and bricks
+    play_button = Button(screen, "Play")
+    scoreboard = Scoreboard(screen, gs)
+    paddle = Paddle(screen, gs)
+    # bricks in pygame container class 'Group' to manage all brick sprites as one unit.
     bricks = Group()
-    balls = Group()
-    balls.add(ball)
-
+    # also maintain ball in a group, since it makes it easier to restart game with new ball when user loses. also
+    # makes detecting collision between bricks and ball simpler.
+    balls = Group(Ball(screen, gs, paddle))
 
     def check_brick_collision():
-        collisions = pygame.sprite.groupcollide(balls,bricks,False,True)
+        # returns dict of brick sprites that collide with ball. 3rd and 4th parameters are for deleting the collided sprites
+        collisions = pygame.sprite.groupcollide(balls, bricks, False, True)
 
+        # if collisions is not None (no collision)
         if collisions:
+            # get the center x-coord of the ball
             for ball in balls:
                 ball_center_rect = ball.rect.centerx
             for value in collisions.values():
@@ -41,55 +46,57 @@ def run_game():
             gs.score += 5
             scoreboard.create_score()
 
-
-
     def update_ball():
         balls.update()
         check_brick_collision()
         for ball in balls.copy():
-            if ball.rect.top >= gs.screen_height and gs.game_active:
+            # if ball is not present on screen and game is over, have to reset ball settings
+            if ball.rect.top >= gs.screen_height and gs.game_over:
                 balls.remove(ball)
-
                 gs.balls_left -= 1
-                #Reset ball speed
+                # Reset ball speed
                 gs.ball_speed_x = gs.ball_speed_x_holder
-                # To prevent game over sound and ball down from playing at same time
-                if gs.balls_left >1:
+                # To prevent game over sound and ball down sound from playing at same time since
+                if gs.balls_left > 1:
                     effect = pygame.mixer.Sound('sounds/ball_down.wav')
                     effect.play()
-                #Updates text of lives left
+                # Updates text of lives left
                 scoreboard.update_balls()
-                new_ball = Ball(screen,gs,paddle)
+                new_ball = Ball(screen, gs, paddle)
                 balls.add(new_ball)
 
-        #Level1
-    def setup_bricks():#4,9
+    #Level1 (4x9)
+    def setup_bricks():
+        # each row will have 9 bricks
         for row in range(4):
-            for column in range(0,9):
-                brick = Brick(gs,gs.colors[row],column * (gs.brick_width+gs.marginx)+12,gs.marginy)
+            for column in range(0, 9):
+                # create new brick
+                brick = Brick(gs, gs.colors[row], column * (gs.brick_width+gs.marginx)+12, gs.marginy)
                 bricks.add(brick)
 
+            # update y marin as next row will be placed down
             gs.marginy += gs.brick_length + 5
 
-    #Level2
-    def setup_bricks2():#8,4
+    # Level2 (8x4)
+    def setup_bricks2():
         for row in range(8):
             for column in range(4):
-                brick = Brick(gs,gs.colors[row],column * (gs.brick_width+gs.marginx2)+40 ,gs.marginy)
+                brick = Brick(gs, gs.colors[row], column * (gs.brick_width+gs.marginx2)+40, gs.marginy)
                 bricks.add(brick)
             gs.marginy += gs.brick_length + 5
-    #Level3  6,5
+
+    # Level3  (6x5)
     def setup_bricks3():
         for row in range(6):
             for column in range(5):
-                brick = Brick(gs,gs.colors[row],column * (gs.brick_width*2) + 40, gs.marginy2)
+                brick = Brick(gs, gs.colors[row], column * (gs.brick_width*2) + 40, gs.marginy2)
                 bricks.add(brick)
             gs.marginy2 += gs.brick_length * 2
         gs.marginy2 = gs.marginy_holder
 
         for row in range(5):
             for column in range(4):
-                brick = Brick(gs,gs.colors[row], column * (gs.brick_width*2) + 40 + gs.brick_width, gs.marginy2 + gs.brick_length)
+                brick = Brick(gs, gs.colors[row], column * (gs.brick_width*2) + 40 + gs.brick_width, gs.marginy2 + gs.brick_length)
                 bricks.add(brick)
             gs.marginy2 += gs.brick_length*2
         gs.marginy2 = gs.marginy_holder
@@ -117,26 +124,11 @@ def run_game():
             gs.marginy += gs.brick_length + 5
         gs.marginy = gs.marginy_holder
 
-    """
-    def setup_bricks5():
-
-        for row in range(5):
-            for column in range(9,0,-2):
-                brick = Brick(gs, gs.colors[row],(n+1) *(gs.brick_width+ gs.marginx)+12, gs.marginy)
-                bricks.add(brick)
-            gs.marginy += gs.brick_length + 5
-    """
-
     def change_level():
-        if gs.level==2:
-            setup_bricks2()
-        if gs.level==3:
-            setup_bricks3()
-        if gs.level==4:
-            setup_bricks4()
-        if gs.level == 5:
-            setup_bricks5()
-            # Checks for paddle movement input(arrow keys) and resets game from input when game has ended.
+        # run the setup_bricks corresponding to the level
+        eval('setup_bricks' + str(gs.level))
+
+    # Checks for paddle movement input(arrow keys) and resets game from input when game has ended.
     def reset():
         setup_bricks()
         #Resetting marginy so it does not draw lower
@@ -147,7 +139,7 @@ def run_game():
         gs.score = gs.score_holder
         scoreboard.create_score()
         gs.ball_speed_x = gs.ball_speed_x_holder
-        gs.game_active = True
+        gs.game_over = True
         gs.balls_left = gs.total_balls
         gs.play_sound = True
 
@@ -157,13 +149,13 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT and gs.game_active:
+                if event.key == pygame.K_RIGHT and gs.game_over:
                     paddle.paddle_moving_right = True
-                elif event.key == pygame.K_LEFT and gs.game_active:
+                elif event.key == pygame.K_LEFT and gs.game_over:
                     paddle.paddle_moving_left = True
                 # Reset game input
-                elif event.key == pygame.K_SPACE and not gs.game_active:
-                    gs.game_active = True
+                elif event.key == pygame.K_SPACE and not gs.game_over:
+                    gs.game_over = True
                     reset()
                 # Exit game
                 elif event.key == pygame.K_q:
@@ -175,22 +167,22 @@ def run_game():
                 elif event.key == pygame.K_LEFT:
                     paddle.paddle_moving_left = False
 
-            if event.type == pygame.MOUSEBUTTONDOWN and not gs.game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN and not gs.game_over:
                 mousex, mousey = pygame.mouse.get_pos()
                 button_clicked = play_button.rect.collidepoint(mousex, mousey)
                 if button_clicked:
-                    gs.game_active = True
+                    gs.game_over = True
                     reset()
     #Game loop
     while True:
         check_events()
         #Updates
-        if gs.game_active:
+        if gs.game_over:
             update_ball()
             paddle.update_paddle()
         #drawing
         screen.fill(gs.background_color)
-        if not gs.game_active:
+        if not gs.game_over:
             play_button.draw_buttons()
         paddle.draw_paddle()
         bricks.draw(screen)
@@ -209,7 +201,7 @@ def run_game():
 
 
 
-            gs.game_active = False
+            gs.game_over = False
             gs.first_start = True
             check_events()
             scoreboard.update_balls()
@@ -224,6 +216,7 @@ def run_game():
             pygame.time.delay(1000)
             #FIGURE OUT HOW TO CENTER PADDLE
             gs.level += 1
-run_game()
 
 
+if __name__ == '__main__':
+    run_game()
